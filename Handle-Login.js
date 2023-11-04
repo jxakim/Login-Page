eb6cde2954b64d49a2389e4faa08e8d346ea58e9
Joakim <joakim.solvang.christiansen@gmail.com>
2023-11-04 04:48:27 +0100

    Got all main files up, started roughly on the code

---
+++ Handle-Login.js
@@ -0,0 +1,41 @@
+﻿const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const usernameInput = document.querySelector("#username");
+    const passwordInput = document.querySelector("#password");
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // Get the data from the datastorage file
+    fetch("datastorage.txt")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to fetch the file: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            let dataList = data.split("\n");
+            let matchFound = false;
+
+            // Match the data from the file with the credentials from the form
+            for (let i = 0; i < dataList.length - 1; i++) {
+                let userData = dataList[i].split(",");
+                if (username == userData[0] && password === userData[1]) {
+                    document.getElementById("output").innerHTML = "Velkommen!";
+                    matchFound = true;
+                    break;
+                }
+            }
+
+            if (!matchFound) {
+                document.getElementById("output").innerHTML = "Feil brukernavn eller passord.";
+            }
+        })
+        .catch(error => {
+            document.getElementById("output").innerHTML = `An error occurred: ${error}`;
+        });
+});
